17e4bd4fb70eeefc8b20af3d361326a20e714db7
Jongsoo Yoon <jongsoojong@gmail.com>
2018-03-02 19:51:41 -0800

    [add] Add the Youtube Component to App Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Timer from './Components/Timer-Component.js';
-import CookieClicker from './Components/Cookie-Clicker.js'
+import CookieClicker from './Components/Cookie-Clicker.js';
+import Youtube from './Components/Youtube-Component.js';
 import './App.css';
 import axios from 'axios';
 
@@ -8,35 +9,11 @@ class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      returnedVideos: []
+    
     }
   }
 
   componentDidMount() {
-    this.retrieveYoutubeVideos();
-  }
-
-  retrieveYoutubeVideos = (that = this, userSearchQuery) => {
-    axios.post('/retrieveYoutubeFeed', {
-      // you should replace this query with the userSearchQuery's value you will provide
-      searchQuery: 'yogscast'
-    })
-    .then(function (response) {
-      let newReturnedVideos = that.buildNewVideosArray(response.data);
-      that.setState({
-        returnedVideos: newReturnedVideos,
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  buildNewVideosArray = (responseData) => {
-    return responseData.map(function(video){
-      let videoURL = "http://www.youtube.com/embed/" + video.id.videoId;
-      return <iframe className="ytplayer" type="text/html" width="640" height="360" src={videoURL} frameBorder="0"></iframe>;
-    });
   }
 
   getUsers = () => {
@@ -81,10 +58,7 @@ class App extends Component {
       <div className="App">
         <Timer/>
         <CookieClicker/>
-        <div>{this.state.returnedVideos}</div>
-        <button style={{'position': 'absolute', 'top': '100px', 'zIndex': 4}} onClick={() => {this.addUserToUsersTable()}}>{'Add user'}</button>
-        <button style={{'position': 'absolute', 'top': '200px', 'zIndex': 4}} onClick={() => {this.verifyUser()}}>{'Verify user'}</button>
-        <button style={{'position': 'absolute', 'top': '300px', 'zIndex': 4}} onClick={() => {this.getUsers()}}>{'Get Users'}</button>        
+        <Youtube />
       </div>
     );
   }
--- src/Components/Youtube-Component.js
+++ src/Components/Youtube-Component.js
@@ -1,1 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 
+class Youtube extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        returnedVideos: []
+      }
+    }
+
+    componentDidMount() {
+        this.retrieveYoutubeVideos();
+    }
+
+    retrieveYoutubeVideos = (that = this, userSearchQuery) => {
+      axios.post('/retrieveYoutubeFeed', {
+      // you should replace this query with the userSearchQuery's value you will provide
+      searchQuery: 'Zangief Kid'
+    })
+    .then(function (response) {
+      let newReturnedVideos = that.buildNewVideosArray(response.data);
+      that.setState({
+        returnedVideos: newReturnedVideos,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    }
+
+    buildNewVideosArray = (responseData) => {
+    return responseData.map(function(video){
+      let videoURL = "http://www.youtube.com/embed/" + video.id.videoId;
+      return <iframe className="ytplayer" type="text/html" width="640" height="360" src={videoURL} frameBorder="0"></iframe>;
+    });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2> Youtube component </h2>
+                <div>{this.state.returnedVideos}</div>
+            </div>
+        )
+    }
+
+}
+
+export default Youtube;
